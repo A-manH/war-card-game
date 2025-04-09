b11ae6b69bf18696166998d0fd743288e5f82334
A-manH <amanhuss3n@gmail.com>
2025-04-09 12:57:49 -0400

    moved comments to seperate file

---
+++ war-card-game/notes.py
@@ -0,0 +1,15 @@
+#create dictionary of 52 cards of 13 spade, heat, diamond, & club
+    #for loop each type
+    #create dictionary for each key that has rank, type, and number
+#randomize deck each round and split and append to both players
+#choose random player to start
+#show card of starting player then show card of other player
+    #display last index of players deck
+    #evaluate the greate card
+        #compare deck[suite].get(rank)
+    #return both cards to player who won the evaluation
+#if card rank of both players are ==: declare war
+    #get first 4 indices of both decks
+    #compare 4th index
+    #winner gets all cards appended to their deck
+#if deck of 1 player runs out declare other player winner
