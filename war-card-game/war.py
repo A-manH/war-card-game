bae3f82a5380158c6770ade9388d049917785d3d
A-manH <amanhuss3n@gmail.com>
2025-04-09 12:52:43 -0400

    completed comment planning

--- war-card-game/war.py
+++ war-card-game/war.py
@@ -1,1 +1,17 @@
-print("This is my first reposite!")
+deck = {"ace": {"rank": 1, "number": 5}}
+print(deck["ace"].get("rank"))
+#create dictionary of 52 cards of 13 spade, heat, diamond, & club
+    #for loop each type
+    #create dictionary for each key that has rank, type, and number
+#randomize deck each round and split and append to both players
+#choose random player to start
+#show card of starting player then show card of other player
+    #display last index of players deck
+    #evaluate the greate card
+        #compare rank
+    #return both cards to player who won the evaluation
+#if card rank of both players are ==: declare war
+    #get first 4 indices of both decks
+    #compare 4th index
+    #winner gets all cards appended to their deck
+#if deck of 1 player runs out declare other player winner
