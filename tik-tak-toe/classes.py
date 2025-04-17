1bb6c4ff6b2c148067ac1d7d5933adf1cdd2b8f6
A-manH <amanhuss3n@gmail.com>
2025-04-17 16:53:32 -0400

    fix: made playing board attribute of board fixed logical bug

--- tik-tak-toe/classes.py
+++ tik-tak-toe/classes.py
@@ -1,27 +1,9 @@
-players = []
-grid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-playing_board = grid.copy()
 class Board():
     def __init__(self):
-        pass
-
-    def new_board(self):
-        counter = 0
-
-        for row in range(3):
-            if row in range(3):
-                print("-------------")
-
-            for i in range(3):
-                print(f"| {grid[counter]} ", end="")
-                counter += 1
-                if i == 2:
-                    print("|", end="")
-
-            if row == 2:
-                print("\n-------------")
-            print()
-
+        self.players = []
+        self.grid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.playing_board = self.grid.copy()
+        
     def visualize(self):
         counter = 0
 
@@ -30,7 +12,7 @@ class Board():
                 print("-------------")
 
             for i in range(3):
-                print(f"| {playing_board[counter]} ", end="")
+                print(f"| {self.playing_board[counter]} ", end="")
                 counter += 1
                 if i == 2:
                     print("|", end="")
@@ -39,46 +21,51 @@ class Board():
                 print("\n-------------")
             print()
 
-    def update(self, square, move):
-        playing_board[square] = move
-        self.visualize()
-
     def check_winner(self, player, opponenet):
-        solution_1 = playing_board[:3]
-        solution_2 = playing_board[3:6]
-        solution_3 = playing_board[6:9]
+        solution_1 = self.playing_board[:3]
+        solution_2 = self.playing_board[3:6]
+        solution_3 = self.playing_board[6:9]
         horizontal_win = [solution_1, solution_2, solution_3]
 
-        solution_4 = playing_board[:7:3]
-        solution_5 = playing_board[1:8:3]
-        solution_6 = playing_board[2:9:3]
+        solution_4 = self.playing_board[:7:3]
+        solution_5 = self.playing_board[1:8:3]
+        solution_6 = self.playing_board[2:9:3]
         vertical_win = [solution_4, solution_5, solution_6]
 
-        solution_7 = playing_board[:9:4]
-        solution_8 = playing_board[2:7:2]
+        solution_7 = self.playing_board[:9:4]
+        solution_8 = self.playing_board[2:7:2]
         diagonal_win = [solution_7, solution_8]
 
         for win in (horizontal_win, vertical_win, diagonal_win):
             for solution in win:
                 if all(i == "X" for i in solution):
-                    if player.move == "X" or player.move == "O":
+                    if player.move == "X":
                         player.score += 1
                         print(f"Solution found! Winner is {player.name}!")
                         print(f"Score is: {player.score} - {opponenet.score}")
-                        self.new_board()
+                        self.reset()
+                elif all(i == "O" for i in solution):
+                        player.score += 1
+                        self.reset()
+                        print(f"Solution found! Winner is {player.name}!")
+                        print(f"\tScore is: {player.score} - {opponenet.score}")
 
     def reset(self):
         self.new_board()
-        for p in players:
-            p.move = None
+        for player in self.players:
+            player.move = None
+
+    def new_board(self):
+        self.playing_board = self.grid.copy()
+
+    def update(self, square, move):
+        self.playing_board[square] = move
 
 class Player():
     def __init__(self, name, move="X/O"):
         self.name = name
         self.move = None
         self.score = 0
-        players.append(self)
-
 
     def play_move(self, board, opponent):
         square = int(input(f"{self.name}, pick a sqaure: "))
@@ -92,6 +79,5 @@ class Player():
                     print("Opponent is playing O, play X")
             else:
                 self.move = move
-                Board().update(square, move)
-                board.check_winner(self)
+                board.update(square, move)
                 break
